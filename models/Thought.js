f4bb2c1554f96d38968b10495a2d488f6982059e
tooqk4u <tooqk4u@protonmail.com>
2022-04-03 15:22:47 -0700

    fixed thought model

--- models/Thought.js
+++ models/Thought.js
@@ -1,44 +1,46 @@
-const { Schema, model } = require('mongoose');
-const dateFormat = require('../utils/dateFormat');
+const { Schema, model, Types } = require("mongoose");
+const dateFormat = require('../utils/dateFormat')
 
 const ReactionSchema = new Schema(
-    {
-        reactionId: {
-            type: Schema.Types.ObjectId,
-            default: Schema.Types.ObjectId
-        },
-        reactionBody: {
-            type: String,
-            required: true,
-            maxlength: 280,
-            minlength: 1,
-        },
-        username: {
-          type: String,
-          required: true
-      },
-      createdAt: {
-          type: Date,
-          default: Date.now(),
-          get: createdAtVal => dateFormat(createdAtVal)
-      }
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+
+    reactionBody: {
+      type: String,
+      required: true,
+      min: 1,
+      max: 280,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+      get: (createdAtVal) => dateFormat(createdAtVal)
+    },
+    
   },
   {
     toJSON: {
-      getters: true
+      getters: true,
     },
-     // prevents virtuals from creating duplicate of _id as `id`
+// prevents virtuals from creating duplicate of _id as `id`
     id: false
-  }, 
-
+  },
 );
+
 const ThoughtSchema = new Schema(
   {
     thoughtText: {
       type: String,
-      requierd: true,
-      minlenght: 1,
-      maxlenght: 280,
+      required: true,
+      min: 1,
+      max: 280,
       trim: true,
     },
 
@@ -71,7 +73,7 @@ ThoughtSchema.virtual("reactionCount").get(function () {
   return this.reactions.length;
 });
 
-//Create the Model for Thought
+//Create the Thought Model
 const Thought = model("Thought", ThoughtSchema);
 
 
